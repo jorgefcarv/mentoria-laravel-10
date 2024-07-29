bc52793b965079368ea8af01f275f311183ffec6
jorgefcarv <jorgefcarv@gmail.com>
2024-07-28 21:09:43 -0300

    CRUD PRODUTOS - formulários

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\FormRequestProduto;
+use App\Models\Componentes;
 use App\Models\Produto;
+use Brian2694\Toastr\Facades\Toastr;
 use Illuminate\Http\Request;
-
-use function Laravel\Prompts\search;
 
 class ProdutosController extends Controller
 {
@@ -13,15 +14,55 @@ class ProdutosController extends Controller
     {
         $this->produto = $produto;
     }
-    public function index (Request $request) {
 
+    public function index(Request $request)
+    {
         $pesquisar = $request->pesquisar;
         $findProduto = $this->produto->getProdutosPesquisarIndex(search: $pesquisar ?? '');
 
-        return view('pages.produtos.paginacao', compact ('findProduto'));
+        return view('pages.produtos.paginacao', compact('findProduto'));
     }
-    public function delete (Request $request)
+
+    public function delete(Request $request)
     {
-        
+        $id = $request->id;
+        $buscaRegistro = Produto::find($id);
+        $buscaRegistro->delete();
+
+        return response()->json(['success' => true]);
+    }
+
+    public function cadastrarProduto(FormRequestProduto $request)
+    {
+        if ($request->method() == "POST") {
+            // cria os dados
+            $data = $request->all();
+            $componentes = new Componentes();
+            $data['valor'] = $componentes->formatacaoMascaraDinheiroDecimal($data['valor']);
+            Produto::create($data);
+
+            Toastr::success('Dados gravados com sucesso.');
+            return redirect()->route('produto.index');
+        }
+        // mostrar os dados
+        return view('pages.produtos.create');
+    }
+
+    public function atualizarProduto(FormRequestProduto $request, $id)
+    {
+        if ($request->method() == "PUT") {
+            // atualiza os dados
+            $data = $request->all();
+            $componentes = new Componentes();
+            $data['valor'] = $componentes->formatacaoMascaraDinheiroDecimal($data['valor']);
+            $buscaRegistro = Produto::find($id);
+            $buscaRegistro->update($data);
+
+            Toastr::success('Dados atualizados com sucesso.');
+            return redirect()->route('produto.index');
+        }
+        $findProduto = Produto::where('id', '=', $id)->first();
+
+        return view('pages.produtos.atualiza', compact('findProduto'));
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -106,12 +106,14 @@
 
     @yield('scripts')
     <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-    <script src="/js/bootstrap.bundle.min.js"></script>
+    <script src="/js/bootstrap.bLundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js"></script>
     <script src="/js/dashboard.js"></script>
     <script src="/js/color-modes.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.10/jquery.mask.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.blockUI/2.70/jquery.blockUI.js"></script>
+    <script src="/js/projeto.js"></script>
 
   </body>
 </html>
