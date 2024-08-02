a0e8486ce3991d40259b9042adade67de4e90ba0
jorgefcarv <jorgefcarv@gmail.com>
2024-08-02 14:33:37 -0300

    CRUD Vendas

--- app/Http/Controllers/VendaController.php
+++ app/Http/Controllers/VendaController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\FormRequestVenda;
+use App\Models\Cliente;
+use App\Models\Produto;
 use App\Models\Venda;
 use Brian2694\Toastr\Facades\Toastr;
 use Illuminate\Http\Request;
@@ -22,26 +24,22 @@ class VendaController extends Controller
         return view('pages.vendas.paginacao', compact('findVendas'));
     }
 
-    public function delete(Request $request)
-    {
-        $id = $request->id;
-        $buscaRegistro = Venda::findVendas($id);
-        $buscaRegistro->delete();
-
-        return response()->json(['success' => true]);
-    }
-
     public function cadastrarVenda(FormRequestVenda $request)
-    {
+    {   
+        $findNumeracao = Venda::max('numero_da_venda') + 1;
+        $findProduto = Produto::all();
+        $findCliente = Cliente::all();
+
         if ($request->method() == "POST") {
             // cria os dados
             $data = $request->all();
+            $data['numero_da_venda'] = $findNumeracao;
             Venda::create($data);
 
             Toastr::success('Gravado com sucesso');
             return redirect()->route('vendas.index');
         }
-        // mostrar os dados
-        return view('pages.vendas.create');
+
+        return view('pages.vendas.create', compact('findNumeracao', 'findProduto', 'findCliente'));
     }
 }
