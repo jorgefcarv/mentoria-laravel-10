a82d87fe44f852fd29ca11b61d6c08ed08d0b945
jorgefcarv <jorgefcarv@gmail.com>
2024-08-04 13:30:43 -0300

    CRUD Clientes

--- resources/views/pages/clientes/atualiza.blade.php
+++ resources/views/pages/clientes/atualiza.blade.php
@@ -5,7 +5,7 @@
         @csrf
         @method('PUT')
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-            <h1 class="h2">Editar Produto</h1>
+            <h1 class="h2">Editar Cliente</h1>
         </div>
         <div class="mb-3">
             <label class="form-label">Nome</label>
